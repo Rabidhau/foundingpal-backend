6227f6c6b8262e935c76bdf73fc76bcafb7f5645
Rabidhau01 <np03cs4a220058@heraldcollege.edu.np>
2025-04-15 12:26:23 +0545

    some minor improvements✨

--- src/controllers/showidea.js
+++ src/controllers/showidea.js
@@ -1,20 +1,29 @@
 const conn = require("../db/connection");
 
 const getIdeaById = async (req, res) => {
-  const ideaId = req.params.id; // pass jobId as a parameter in the URL
+  const ideaId = req.params.id;
 
   try {
-    const selectQuery = "SELECT * FROM idealisting WHERE id = ?";
+    const selectQuery = `
+      SELECT 
+        i.*, 
+        u.*
+      FROM idealisting i
+      JOIN Users u ON i.userId = u.userId
+      WHERE i.id = ?
+    `;
+
     conn.query(selectQuery, [ideaId], (err, result) => {
       if (err) {
-        console.error("Error fetching job from db:", err);
-        res.status(400).send("Error fetching job from database");
+        console.error("Error fetching idea and founder info from db:", err);
+        res.status(400).send("Error fetching idea and founder info from database");
       } else {
         if (result.length === 0) {
           res.status(404).send("Idea not found");
         } else {
-          const idea = result[0];
-          res.status(200).send(idea);
+          const ideaWithFounderInfo = result[0];
+          console.log("Fetched idea with founder info:", ideaWithFounderInfo);
+          res.status(200).send(ideaWithFounderInfo);
         }
       }
     });
